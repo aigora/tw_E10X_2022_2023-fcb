81b1f4ef72386f5d36097c9ae6a0af35042d2cf1
Gabriel-S-J <gabriel.santos@alumnos.upm.es>
2023-05-29 18:13:09 +0200

    cambio unidades

--- FINAL TRABAJO/energias.h
+++ FINAL TRABAJO/energias.h
@@ -423,7 +423,7 @@ printf(".........BIENVENIDO A LA BASE DE DATOS............\n\n");
                             for (int i = 0; i <FILAS ; i++) {
                              printf("Titulo: %s\n", registros[i].titulo);
                               for (int j = 0; j <= 11; j++) {
-                             printf("%d/2021: %f GW \n", j+1, registros[i].datos[j]);
+                             printf("%d/2021: %f MWh \n", j+1, registros[i].datos[j]);
                                                           }
                                             printf("\n\n\n");
                                            }//bucle que recorre cada año y lo imprime por pantalla
@@ -436,7 +436,7 @@ printf(".........BIENVENIDO A LA BASE DE DATOS............\n\n");
                              printf("Titulo: %s\n", registros[i].titulo);
                               for (int j = 12; j < COLUMNAS; j++) {
 
-                             printf("%d/2022: %f GW \n",j-11, registros[i].datos[j]);
+                             printf("%d/2022: %f MWh \n",j-11, registros[i].datos[j]);
 
                                                           }
                                             printf("\n\n\n");
